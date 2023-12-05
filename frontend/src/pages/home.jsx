885fc014d62fc27a78db197d5a65af5c8145bb19
Sebastian <josbat900@gmail.com>
2023-12-04 19:47:38 -0500

    empezando el front

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,43 @@
+import React,{useEffect,useState} from 'react'
+import axios from "axios"
+import Spinner from '../componets/Spinner'
+import {Link} from "react-router-dom"
+import {AiOutlineEdit} from "react-icons/ai"
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
+const home = () => {
+  const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    axios
+      .get("http://localhost:5555/books")
+      .then((response)=>{
+        setBooks(response.data.data)
+        setLoading(false)
+      })
+      .catch((error)=>{
+        console.error(error)
+        setLoading(false)
+      })
+  }, [])
+  return (
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Book list</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBoox className="text-sky-800 text-4xl"/>
+          {loading ? (
+        <Spinner />
+      ) : (
+        <table className='w-full border-separate border-spacing-2'></table>
+      )}
+        </Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default home
