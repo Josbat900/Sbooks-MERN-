5eb5bac2984c9d8f96f0b89289cd1d84cc73345b
Sebastian <josbat900@gmail.com>
2023-12-09 13:00:51 -0500

    book modal add

--- frontend/src/componets/home/BookSingleCard.jsx
+++ frontend/src/componets/home/BookSingleCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom"
 import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/bi'
+import { BiUserCircle,BiShow } from 'react-icons/bi'
 import { AiOutlineEdit } from "react-icons/ai"
 import { BsInfoCircle } from "react-icons/bs"
 import { MdOutlineDelete } from "react-icons/md"
+import { useState } from "react"
+import BookModal from "./BookModal"
 
 const BookSingleCard = ({ book }) => {
+    const [showModal, setShowmodal] = useState(false)
     return (
-        <div
-            key={book._id}
-            className="border-2 border-sky-300 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
-        >
+        <div className="border-2 border-sky-300 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
             <h2 className="absolute top-1 right-2 px-4 py-1 bg-sky-300 border-sky-400 border-2 rounded-lg m-full">
                 {book.publishYear}
             </h2>
@@ -24,6 +24,9 @@ const BookSingleCard = ({ book }) => {
                 <h2 className="my-1">{book.author}</h2>
             </div>
             <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+                <BiShow className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+                onClick={()=> setShowmodal(true)}
+                />
                 <Link to={`/books/details/${book._id}`}>
                     <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
                 </Link>
@@ -34,6 +37,9 @@ const BookSingleCard = ({ book }) => {
                     <MdOutlineDelete className="text-2xl text-red-600 hover:text_black" />
                 </Link>
             </div>
+            {
+                showModal && (<BookModal book={book} onClose={()=> setShowmodal(false)}/>)
+            }
         </div>
     )
 }
